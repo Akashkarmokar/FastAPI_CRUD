46302a3773ef36451bdeae7fcc4ea4ff8123478a
Akash <akashcsemu@gmail.com>
2023-01-28 22:55:20 +0600

    feat: uvicorn added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import  FastAPI
-
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get('/index')
 async def index():
     return { 'status': 'OK', 'msg': "Data fetched successfully"}
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app)
